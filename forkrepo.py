e86cc30e0a4c1e7f2a3b6b94c8e63c3645c29937
Arpit Jain <arpitjain799@gmail.com>
2023-04-26 13:06:22 +0900

    add file

--- forkrepo.py
+++ forkrepo.py
@@ -5,7 +5,7 @@ import subprocess
 import yaml
 import time
 
-GITHUB_TOKEN = os.environ['GITHUB_TOKEN']
+GITHUB_TOKEN = os.environ['GH_API_TOKEN']
 GITHUB_USERNAME = "arpitjain799"
 
 command = ['rm', '-rf', "clonerepo/test/"]
